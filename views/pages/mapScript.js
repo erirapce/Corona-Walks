a482b74b0268e9df9e9d2eee8e0856d1270d6987
erirapce <hermesrapce@gmail.com>
2021-01-13 15:57:39 +0100

    add hard-coded data

--- views/pages/mapScript.js
+++ views/pages/mapScript.js
@@ -8,10 +8,42 @@ var mymap = L.map('mapid').setView([52.52, 13.41], 13);
               accessToken: 'your.mapbox.access.token'
             }).addTo(mymap);
 
-var circle = L.circle([52.52, 13.41],{
+var circle1 = L.circle([52.52, 13.41],{
     color: 'cyan',
     fillColor: 'cyan',
     fillOpacity: 0.5,
-    radius: 500
+    radius: 75
 }).addTo(mymap);
-circle.bindPopup("Rosenthaler Str. 51, 10178 Berlin, Deutschland");
+circle1.bindPopup("Karl-Liebknecht-Str. 13, 10178 Berlin Deutschland");
+
+var circle2 = L.circle([52.51, 13.40],{
+  color: 'red',
+  fillColor: 'red',
+  fillOpacity: 0.5,
+  radius: 75
+}).addTo(mymap);
+circle2.bindPopup("Berliner Innenstadt, 10178 Berlin, Deutschland");
+
+var circle3 = L.circle([52.53, 13.40],{
+  color: 'blue',
+  fillColor: 'blue',
+  fillOpacity: 0.5,
+  radius: 75
+}).addTo(mymap);
+circle3.bindPopup("Rosenthaler Str. 50, 10178 Berlin, Deutschland");
+
+var circle4 = L.circle([52.509, 13.42],{
+  color: 'orange',
+  fillColor: 'orange',
+  fillOpacity: 0.5,
+  radius: 75
+}).addTo(mymap);
+circle4.bindPopup("Grunerstraße 20, 10179 Berlin, Deutschland");
+
+var circle5 = L.circle([52.515, 13.43],{
+  color: 'blue',
+  fillColor: 'blue',
+  fillOpacity: 0.5,
+  radius: 75
+}).addTo(mymap);
+circle5.bindPopup("Karl-Marx-Allee 8, 10178 Berlin, Deutschland");
