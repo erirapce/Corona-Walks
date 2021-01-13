e1ec3a5e719e8fb4365a342f3f4ab954032c0ad0
erirapce <hermesrapce@gmail.com>
2021-01-13 15:36:30 +0100

    add circle to the map/hardcore data

--- views/pages/mapScript.js
+++ views/pages/mapScript.js
@@ -1,9 +1,16 @@
 var mymap = L.map('mapid').setView([52.52, 13.41], 13);
-L.tileLayer('images/Map.PNG', {
-attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-maxZoom: 18,
-id: 'mapbox/streets-v11',
-tileSize: 512,
-zoomOffset: -1,
-accessToken: 'your.mapbox.access.token'
+            L.tileLayer('images/Map.png', {
+              attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+              maxZoom: 18,
+              id: 'mapbox/streets-v11',
+              tileSize: 512,
+              zoomOffset: -1,
+              accessToken: 'your.mapbox.access.token'
+            }).addTo(mymap);
+
+var circle = L.circle([52.52, 13.41],{
+    color: 'cyan',
+    fillColor: 'cyan',
+    fillOpacity: 0.5,
+    radius: 500
 }).addTo(mymap);
